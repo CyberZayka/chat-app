83f3683544ab46f62fc4be11f8b7e930cc9c5fb5
oleksii <isaevaleksej196@gmail.com>
2023-02-09 21:54:52 +0200

    added routing

--- chat-client/src/App.js
+++ chat-client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
+import LoginPage from './authPages/LoginPage/LoginPage';
+import RegisterPage from './authPages/RegisterPage/RegisterPage';
+import Dashboard from './Dashboard/Dashboard';
 
 function App() {
-  return (
-    <div className="App" />
-  );
+  return <>
+    <Router>
+      <Routes>
+        <Route exact path='/login' element={<LoginPage />} />
+        <Route exact path='/register' element={<RegisterPage />} />
+        <Route exact path='/dashboard' element={<Dashboard />} />
+        <Route path='/' element={<Navigate to='/dashboard' />} />
+      </Routes>
+    </Router>
+  </>
 }
 
 export default App;
